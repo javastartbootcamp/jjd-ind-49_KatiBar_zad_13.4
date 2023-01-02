7ace836b43d905929dac36c37851ee3725ce613c
KatiBar <katarzyna.kinga.baranska@gmail.com>
2023-01-02 20:47:11 +0100

    Zainicjowanie tablicy na 10 elementów i podwajanie jej rozmiaru, gdy zabraknie miejsca.

--- src/main/java/pl/javastart/task/CustomList.java
+++ src/main/java/pl/javastart/task/CustomList.java
@@ -5,46 +5,35 @@ import java.util.Arrays;
 // Uzupełnij klasę. Możesz ją dowolnie modyfikować.
 // Celem jest, żeby przechodziły testy w src/test/java/[...]/CustomListTest
 public class CustomList<T> {
-    private T[] elements;
+    private int initSize = 10;
+    private T[] elements = (T[]) new Object[initSize];
     private int size = 0;
 
     private void checkOrEnoughCapacity(int capacity) {
-        if (elements == null) {
-            elements = (T[]) new Object[1];
-        } else {
-            if (capacity > elements.length) {
-                int newSize = size + 1;
-                elements = Arrays.copyOf(elements, newSize);
-            }
+        if (capacity > elements.length) {
+            int newSize = size * 2;
+            elements = Arrays.copyOf(elements, newSize);
         }
     }
 
     public void add(T element) {
         checkOrEnoughCapacity(size + 1);
-        if (element != null) {
-            elements[size] = element;
-            size++;
-        }
+        elements[size] = element;
+        size++;
     }
 
     public void add(int index, T element) {
         checkOrEnoughCapacity(size + 1);
-        if (element != null) {
-            T[] newArray = Arrays.copyOfRange(elements, index, size);
-            elements[index] = element;
-            size++;
-            for (int i = index + 1; i < size; i++) {
-                elements[i] = newArray[i - index - 1];
-            }
+        T[] newArray = Arrays.copyOfRange(elements, index, size);
+        elements[index] = element;
+        size++;
+        for (int i = index + 1; i < size; i++) {
+            elements[i] = newArray[i - index - 1];
         }
     }
 
     public int size() {
-        if (elements == null) {
-            return 0;
-        } else {
-            return elements.length;
-        }
+        return size;
     }
 
     public T get(int i) {
@@ -52,18 +41,24 @@ public class CustomList<T> {
     }
 
     public void remove(int index) {
-        if (elements != null) {
-            T[] rightArray = Arrays.copyOfRange(elements, index + 1, elements.length);
-            elements = Arrays.copyOf(elements, elements.length - 1);
-            for (int i = index; i < elements.length; i++) {
-                elements[i] = rightArray[i - index];
-            }
-            size--;
-        }
+        System.arraycopy(elements, index + 1, elements, index, elements.length - index - 1);
+        size--;
     }
 
     @Override
     public String toString() {
-        return Arrays.toString(elements);
+        int notNullElements = checkHowManyNotNullElements();
+        T[] elementsToPrint = Arrays.copyOf(elements, notNullElements);
+        return Arrays.toString(elementsToPrint);
+    }
+
+    private int checkHowManyNotNullElements() {
+        int count = 0;
+        for (T element : elements) {
+            if (element != null) {
+                count++;
+            }
+        }
+        return count;
     }
 }
